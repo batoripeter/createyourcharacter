2e2aba676b9cd442ad17a71cbee88af45dd0fe75
batoripeter <batorifoto@gmail.com>
2023-06-28 12:58:04 +0200

    gif frame removed

--- script.ts
+++ script.ts
@@ -15,6 +15,8 @@ let myEventListener = (event:Event) => {
     let gender = ((genderInput as HTMLInputElement).value)
     let race = ((raceInput as HTMLInputElement).value)
     let myImg = document.querySelectorAll("img")
+    myImg[0].src=""
+
         if ((gender === "male") && (race === "caucasian")) {
             myImg[0].src = "https://carbon-media.accelerator.net/0000000i7zK/cjL2ICbSaZkbltIIFpzTe6;original.gif"
         }   
@@ -32,12 +34,12 @@ let myEventListener = (event:Event) => {
 
             }
 
-           if ((gender === "female") && (race === "african")) {
-            myImg[0].src = "https://64.media.tumblr.com/240265973e4612baa177af3e895eda42/tumblr_inline_nxm9w60dJ51sy4zww_500.gifv"
+           if ((gender === "female") && (race === "fictional")) {
+            myImg[0].src = "https://media.tenor.com/C9wENJzY6fEAAAAi/tails-cute.gif"
            }
 
-           if ((gender === "male") && (race === "african")) {
-            myImg[0].src = "https://carbon-media.accelerator.net/0000000i7zK/e5e0mZ1190TbrmHfoIPo1F;original.gif"
+           if ((gender === "male") && (race === "fictional")) {
+            myImg[0].src = "https://media.tenor.com/gGWbowK_RwoAAAAi/bot-walking.gif"
            }
         }
 
@@ -45,6 +47,7 @@ let myEventListener = (event:Event) => {
         let myThirdEventListener = (event:Event) => {
             let charClass = ((classInput as HTMLInputElement).value)
             let myImg = document.querySelectorAll("img")
+            myImg[1].src=""
             if (charClass === "beginner"){
                 myImg[1].src = "https://img.freepik.com/premium-vector/pixel-art-bitcoin-trophy-cryptocurrency-prize-vector-icon-8bit-game-white-background_360488-929.jpg?size=626&ext=jpg"
                } 
